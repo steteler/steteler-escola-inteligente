36d7b8a7317f795265b493580f4c45fcec6f8b32
steteler <joaovictorkikuti@gmail.com>
2023-11-25 18:25:04 -0300

    feat: terminado requisito 1

--- src/main/java/com/trybe/java/escolainteligente/PessoaEstudante.java
+++ src/main/java/com/trybe/java/escolainteligente/PessoaEstudante.java
@@ -8,14 +8,31 @@ public class PessoaEstudante {
    * Método calcularIdadeEmDias.
    */
   public static int calcularIdadeEmDias(int anos, int meses, int dias) {
-    System.out.println("TODO: Implementar a função calcular Idade em Dias.");
-    return 0;
+    return (anos * 365) + (meses * 30) + dias;
   }
 
   /**
    * Método coletarInformacoes.
    */
   public static void coletarInformacoes() {
-    System.out.println("TODO: Solicitar informações da Pessoa Estudante ");
+    Scanner pessoaScanner = new Scanner(System.in);
+
+    System.out.print("Qual o nome da Pessoa Estudante? ");
+    String nome = pessoaScanner.next();
+
+    System.out.println("Qual a sua idade em anos, meses e dias?");
+
+    System.out.print("anos: ");
+    int anos = pessoaScanner.nextInt();
+
+    System.out.print("meses: ");
+    int meses = pessoaScanner.nextInt();
+
+    System.out.print("dias: ");
+    int dias = pessoaScanner.nextInt();
+
+    pessoaScanner.close();
+
+    System.out.println("A idade de " + nome + " em dias é " + calcularIdadeEmDias(anos, meses, dias) + ".");
   }
 }
