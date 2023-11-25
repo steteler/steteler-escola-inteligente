7be2ed22f03ec4e964a54ff2a609ee6efc165ec2
steteler <joaovictorkikuti@gmail.com>
2023-11-25 20:20:55 -0300

    feat: terminado requisito 2

--- src/main/java/com/trybe/java/escolainteligente/Secretaria.java
+++ src/main/java/com/trybe/java/escolainteligente/Secretaria.java
@@ -8,14 +8,38 @@ public class Secretaria {
    * Método calcularMedia.
    */
   public static double calcularMedia(double nota1, double nota2, double nota3, double nota4) {
-    System.out.println("TODO: Implementar a função calcular média.");
-    return 0;
+    return (nota1 + nota2 + nota3 + nota4) / 4;
   }
 
   /**
    * Método coletarInformacoes.
    */
   public static void coletarInformacoes() {
-    System.out.println("TODO: Solicitar informações para cálculo da média ");
+    Scanner secretariaScanner = new Scanner(System.in);
+
+    System.out.print("Qual o nome da Pessoa Estudante? ");
+    String nome = secretariaScanner.next();
+
+    System.out.print("Entre com as notas das provas:");
+
+    System.out.print("Avaliação 1: ");
+    double nota1 = secretariaScanner.nextDouble();
+
+    System.out.print("Avaliação 2: ");
+    double nota2 = secretariaScanner.nextDouble();
+
+    System.out.print("Avaliação 3: ");
+    double nota3 = secretariaScanner.nextDouble();
+
+    System.out.print("Avaliação 4: ");
+    double nota4 = secretariaScanner.nextDouble();
+
+    double media = calcularMedia(nota1, nota2, nota3, nota4);
+
+    System.out.println(
+        "A média das notas de " + nome + " é " + media
+    );
+
+    secretariaScanner.close();
   }
 }
